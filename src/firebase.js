adc8c797a3d18e5647582ae4bbb18433aa3fa6d9
Rosa Rivas <rivas-rossy@hotmail.com.ar>
2023-06-20 22:59:14 -0300

    Se ajustan configuracion firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyA3oF5YLiIw8DomqYVEDThTQGvsEixXOw4',
-    //authDomain: 'TU_DOMINIO_AUTH',
-    projectId: 'app-egg-12b4a',
-    //storageBucket: 'TU_BUCKET',
-    //messagingSenderId: 'TU_SENDER_ID',
-    appId: 'app-egg-12b4a'
+    apiKey: "AIzaSyA3oF5YLiIw8DomqYVEDThTQGvsEixXOw4",
+    authDomain: "app-egg-12b4a.firebaseapp.com",
+    projectId: "app-egg-12b4a",
+    storageBucket: "app-egg-12b4a.appspot.com",
+    messagingSenderId: "1030906944688",
+    appId: "1:1030906944688:web:df3499056227caf957af75",
+    measurementId: "G-SKXS4W44V8"
 };
 
 const app = initializeApp(firebaseConfig);
